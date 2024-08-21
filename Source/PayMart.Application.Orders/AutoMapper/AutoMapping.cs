7a38b2e30725b077aed58a2ed5fc62682156621b
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-08-20 21:01:29 -0300

    feature/ implementação da biblioteca automapper.

---
+++ Source/PayMart.Application.Orders/AutoMapper/AutoMapping.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PayMart.Domain.Orders.Entities;
+using PayMart.Domain.Orders.Request;
+using PayMart.Domain.Orders.Response.ListOfOrder;
+using PayMart.Domain.Orders.Response.Order;
+
+namespace PayMart.Application.Orders.AutoMapper;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToEntity();
+        EntityToResponse();
+    }
+
+    private void RequestToEntity()
+    {
+        CreateMap<RequestOrder, Order>();
+    }
+
+    private void EntityToResponse()
+    {
+        CreateMap<Order, ResponseOrder>();
+        CreateMap<Order, ResponseList>();
+    }
+}
--- Source/PayMart.Application.Orders/Injection/DependencyInjectionApp.cs
+++ Source/PayMart.Application.Orders/Injection/DependencyInjectionApp.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PayMart.Application.Orders.AutoMapper;
 using PayMart.Application.Orders.UseCases.Post;
 
 namespace PayMart.Application.Orders.Injection;
@@ -8,6 +9,12 @@ public static class DependencyInjectionApp
     public static void AddApplication(this IServiceCollection services)
     {
         AddRepositories(services);
+        AutoMapper(services);
+    }
+
+    public static void AutoMapper(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(AutoMapping));
     }
 
     public static void AddRepositories(IServiceCollection services)
