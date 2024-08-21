228f4b4fd953b72ce38a6b270d0eb20f0bffbb40
Giovanni Lopes <giovannialopes.dev@gmail.com>
2024-08-20 21:06:23 -0300

    feature/ criação da regra de negocio para o metodo Post.

--- Source/PayMart.Application.Orders/UseCases/Post/PostOrderUseCases.cs
+++ Source/PayMart.Application.Orders/UseCases/Post/PostOrderUseCases.cs
@@ -1,12 +1,36 @@
-﻿using PayMart.Domain.Orders.Request;
+﻿using AutoMapper;
+using PayMart.Domain.Orders.Entities;
+using PayMart.Domain.Orders.Interface.Database;
+using PayMart.Domain.Orders.Interface.Orders;
+using PayMart.Domain.Orders.Request;
 using PayMart.Domain.Orders.Response.Order;
 
 namespace PayMart.Application.Orders.UseCases.Post;
 
 public class PostOrderUseCases : IPostOrderUseCases
 {
-    public Task<ResponseOrder> Execute(RequestOrder request)
+    private readonly IMapper _mapper;
+    private readonly IPost _post;
+    private readonly ICommit _commit;
+
+    public PostOrderUseCases(IMapper mapper,
+        IPost post,
+        ICommit commit)
     {
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _post = post;
+        _commit = commit;
+    }
+
+    public async Task<ResponseOrder> Execute(RequestOrder request)
+    {
+        var Order = _mapper.Map<Order>(request);
+
+        await _post.Post(Order);
+
+        await _commit.Commit();
+
+        return _mapper.Map<ResponseOrder>(Order);
+
     }
 }
